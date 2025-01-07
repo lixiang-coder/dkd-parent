4fd0a134b1a8405c6aa5fe5965a051ea66b13ae2
86132 <2894702202@qq.com>
2025-01-07 16:57:06 +0800

    feat:点位管理相关代码

--- dkd-framework/src/main/java/com/dkd/framework/web/exception/GlobalExceptionHandler.java
+++ dkd-framework/src/main/java/com/dkd/framework/web/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.dkd.framework.web.exception;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import com.dkd.common.constant.HttpStatus;
 import com.dkd.common.core.domain.AjaxResult;
 import com.dkd.common.exception.DemoModeException;
 import com.dkd.common.exception.ServiceException;
 import com.dkd.common.utils.StringUtils;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器
@@ -134,5 +139,16 @@ public class GlobalExceptionHandler
     public AjaxResult handleDemoModeException(DemoModeException e)
     {
         return AjaxResult.error("演示模式，不允许操作");
+    }
+
+    /**
+     * 数据完整性异常
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public AjaxResult handelDataIntegrityViolationException(DataIntegrityViolationException e) {
+        if (e.getMessage().contains("foreign")) {
+            return AjaxResult.error("无法删除，有其他数据引用");
+        }
+        return AjaxResult.error("您的操作违反了数据库中的完整性约束");
     }
 }
---
+++ dkd-manage/src/main/java/com/dkd/manage/domain/vo/PartnerVo.java
@@ -0,0 +1,13 @@
+package com.dkd.manage.domain.vo;
+
+import com.dkd.manage.domain.Partner;
+import lombok.Data;
+
+@Data
+public class PartnerVo extends Partner {
+
+    /**
+     * 点位数
+     */
+    private Integer nodeCount;
+}
