0dc541977ea25837f779fef15ce10f38a46f85f0
86132 <2894702202@qq.com>
2025-01-14 12:50:08 +0800

    feat:工单管理--查看工单补货详情

--- dkd-manage/src/main/java/com/dkd/manage/controller/TaskDetailsController.java
+++ dkd-manage/src/main/java/com/dkd/manage/controller/TaskDetailsController.java
@@ -101,4 +101,15 @@ public class TaskDetailsController extends BaseController
     {
         return toAjax(taskDetailsService.deleteTaskDetailsByDetailsIds(detailsIds));
     }
+
+    /**
+     * 查看工单补货详情
+     */
+    @PreAuthorize("@ss.hasPermi('manage:taskDetails:list')")
+    @GetMapping(value = "/byTaskId/{taskId}")
+    public AjaxResult byTaskId(@PathVariable("taskId") Long taskId) {
+        TaskDetails taskDetailsParam = new TaskDetails();
+        taskDetailsParam.setTaskId(taskId);
+        return success(taskDetailsService.selectTaskDetailsList(taskDetailsParam));
+    }
 }
