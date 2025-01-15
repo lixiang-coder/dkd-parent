9c84ba7e7d6ad97691797626248515920290018d
86132 <2894702202@qq.com>
2025-01-15 15:20:10 +0800

    feat:订单管理--代码改造

--- dkd-manage/src/main/java/com/dkd/manage/domain/Order.java
+++ dkd-manage/src/main/java/com/dkd/manage/domain/Order.java
@@ -1,5 +1,6 @@
 package com.dkd.manage.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.dkd.common.annotation.Excel;
@@ -16,6 +17,7 @@ public class Order extends BaseEntity
     private static final long serialVersionUID = 1L;
 
     /** 主键 */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long id;
 
     /** 订单编号 */
